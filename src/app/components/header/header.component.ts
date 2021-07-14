8c36fca67edbff42e3bc1b55492382b12438714f
tmm-michalski <tmm.michalski@gmail.com>
2021-07-14 19:38:22 +0200

    Added ui.service to toggle addTask component visibility

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'Task Tracker';
+    title: string = 'Task Tracker';
+    showAddTask: boolean;
+    subscription: Subscription;
 
-  constructor() { }
+    constructor(private uiService: UiService) {
+        this.subscription = this.uiService
+            .onToggle()
+            .subscribe((value) => (this.showAddTask = value));
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
-  toggleAddTask() {
-      console.log(`Toggle`);
-      
-  }
-
+    toggleAddTask() {
+        this.uiService.toggleAddTask();
+    }
 }
